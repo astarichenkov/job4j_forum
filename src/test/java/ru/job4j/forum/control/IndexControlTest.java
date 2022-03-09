319a26f7ca1d3a5d3bf9e311ddbd911a6c0b6662
Anton Starichenkov <anton.starichenkov@gmail.com>
2022-03-09 09:25:52 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/forum/control/IndexControlTest.java
+++ src/test/java/ru/job4j/forum/control/IndexControlTest.java
@@ -9,9 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.service.PostService;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -19,6 +21,9 @@ public class IndexControlTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private PostService posts;
 
     @Test
     @WithMockUser
